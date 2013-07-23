5644959517674d423211aa4e9e7b158160e45c70
kirankoduru <crazycreature11@gmail.com>
2013-07-22 22:28:59 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php require_once('header.php');?>
+	<div id="banner">
+		<div class="left">
+			<h2>We design , We code &amp; We share !</h2>
+			<blockquote>
+				" Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+				Curabitur nec metus sit amet felis facilisis vulputate. 
+				Aliquam erat volutpat.Donec elementum sem at elit port "
+			</blockquote>
+			<a href="#"><span class="calltoaction"></span></a>
+		</div>
+		<div class="right">
+			<div class="browserwindow"><img src="images/slide1.jpg"/></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="description">
+		<div class="left">
+			<h2>Responsive Designs</h2>
+			<img src="images/ipadphone.jpg" class="left"/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur 
+				adipiscing elit. Proin convallis purus vitae 
+				ante dignissim eget porttitor lacus bibend
+				um. Ut lorem elit, porttitor ut tempor eget,
+				lobortis sit amet est. Integer congue venen atis porta. 
+				Sed id ipsum id purus dictum dignissim eu ut sapien.  
+			</p>
+		</div>
+		<div class="left">
+			<h2>Optimized Websites</h2>
+			<img src="images/speedometer.jpg" class="left"/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur 
+				adipiscing elit. Proin convallis purus vitae 
+				ante dignissim eget porttitor lacus bibend
+				um. Ut lorem elit, porttitor ut tempor eget,
+				lobortis sit amet est. Integer congue venen atis porta. 
+				Sed id ipsum id purus dictum dignissim eu ut sapien.  
+			</p>
+		</div>
+		<div class="right">
+			<h2>User Experience centric</h2>
+			<img src="images/badge.jpg" class="left"/>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur 
+				adipiscing elit. Proin convallis purus vitae 
+				ante dignissim eget porttitor lacus bibend
+				um. Ut lorem elit, porttitor ut tempor eget,
+				lobortis sit amet est. Integer congue venen atis porta. 
+				Sed id ipsum id purus dictum dignissim eu ut sapien.  
+			</p>
+		</div>
+		<div class="clear border0"></div>
+	</div>
+	<div>
+		<div class="ipad left"><img src="images/superhero.jpg"/></div>
+		<img style="margin-bottom:10px;" src="images/greatban.jpg" alt="great responsibility with great design" class="greattext"/>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis purus vitae ante dignissim eget porttitor lacus bibendum. Ut lorem elit, porttitor ut tempor eget, lobortis sit amet est. Integer congue venenatis porta. Sed id ipsum id purus dictum dignissim eu ut sapien. Aliquam commodo nibh ut neque iaculis molestie. Pellentesque dictum felis vehicula sapien varius congue. Nunc nisi risus, fringilla commodo tempor at, rhoncus et lorem. Cras tincidunt iaculis libero nec gravida. Quisque viverra condimentum nibh, vitae volutpat mi aliquet vitae. Praesent dignissim consectetur nisi, at egestas quam adipiscing vel. Nam vel ipsum enim, vitae egestas urna. Vivamus justo eros, varius tristique interdum in, malesuada id felis. Suspendisse dapibus neque pretium nunc 
+		</p>
+		<div class="clear"></div>
+	</div>
+	
+<?php require_once('footer.php');?>
